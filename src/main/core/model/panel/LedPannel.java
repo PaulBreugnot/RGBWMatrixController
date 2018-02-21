0c1d1b958c7b7f44b23cf8eb9beda43095c7354a
PaulBreugnot <paul.breugnot@etu.emse.fr>
2018-02-21 09:53:39 +0100

    implementing RGBWPixel

---
+++ src/main/core/model/panel/LedPannel.java
@@ -0,0 +1,8 @@
+package main.core.model.panel;
+
+public class LedPannel {
+
+	public static final int MATRIX_WIDTH = 32;
+	public static final int MATRIX_HEIGHT = 16;
+
+}
---
+++ src/main/core/model/pixel/RGBWPixel.java
@@ -0,0 +1,104 @@
+package main.core.model.pixel;
+
+import javafx.scene.paint.Color;
+
+public class RGBWPixel {
+
+	public enum ColorType {
+		RGB, HSB
+	};
+
+	private Color color;
+	private int red;
+	private int green;
+	private int blue;
+	private int white;
+
+	private RGBWPixel(int red, int green, int blue, int white) {
+		color = Color.rgb(red, green, blue);
+		this.red = red;
+		this.green = green;
+		this.blue = blue;
+		this.white = white;
+	}
+
+	private RGBWPixel(double hue, double saturation, double brightness, int white) {
+		color = Color.hsb(hue, saturation, brightness);
+		this.white = white;
+	}
+
+	public static RGBWPixel rgbwPixel(int red, int green, int blue, int white) {
+		return new RGBWPixel(red, green, blue, white);
+	}
+
+	public static RGBWPixel hsbwPixel(double hue, double saturation, double brightness, int white) {
+		return new RGBWPixel(hue, saturation, brightness, white);
+	}
+
+	public static RGBWPixel rgbPixel(int red, int green, int blue) {
+		return rgbwPixel(red, green, blue, 0);
+	}
+
+	public static RGBWPixel hsbPixel(double hue, double saturation, double brightness) {
+		return hsbwPixel(hue, saturation, brightness, 0);
+	}
+
+	public int getRed() {
+		return red;
+	}
+
+	public void setRed(int red) {
+		this.red = red;
+		color = Color.rgb(red, green, blue);
+	}
+
+	public int getGreen() {
+		return green;
+	}
+
+	public void setGreen(int green) {
+		this.green = green;
+		color = Color.rgb(red, green, blue);
+	}
+
+	public int getBlue() {
+		return blue;
+	}
+
+	public void setBlue(int blue) {
+		this.blue = blue;
+		color = Color.rgb(red, green, blue);
+	}
+
+	public double getHue() {
+		return color.getHue();
+	}
+
+	public void setHue(double hue) {
+		color = Color.hsb(hue, color.getSaturation(), color.getBrightness());
+	}
+
+	public double getSaturation() {
+		return color.getSaturation();
+	}
+
+	public void setSaturation(double saturation) {
+		color = Color.hsb(color.getHue(), saturation, color.getBrightness());
+	}
+
+	public double getBrightness() {
+		return color.getBrightness();
+	}
+
+	public void setBrightness(double brightness) {
+		color = Color.hsb(color.getHue(), color.getSaturation(), brightness);
+	}
+
+	public int getWhite() {
+		return white;
+	}
+
+	public void setWhite(int white) {
+		this.white = white;
+	}
+}
