965da862169dd2b3557c5ebdbfe94e8cba689b6a
PaulBreugnot <paul.breugnot@etu.emse.fr>
2018-03-24 16:24:37 +0100

    Creating LoopingAnimations views.

--- src/test/MainTest.java
+++ src/test/MainTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import main.core.model.animations.circularWave.CircularWave;
+import main.core.model.animations.diamondWave.DiamondWave;
+import main.core.model.animations.loopingAnimations.LoopingAnimations;
 import main.core.model.panel.LedPanel;
 import main.gui.app.MatrixControllerApp;
 
@@ -13,6 +16,12 @@ public class MainTest {
 	public static LedPanel getAppConfig() {
 		LedPanel ledPanel = new LedPanel(25);
 		LedPanel.setBlackPanel(ledPanel.getLedMatrix());
+
+		LoopingAnimations loop = new LoopingAnimations();
+		loop.addAnimation(0, new CircularWave());
+		loop.addAnimation(500, new DiamondWave());
+		loop.setEnd(1000);
+		ledPanel.setCurrentAnimation(loop);
 
 		return ledPanel;
 	}
