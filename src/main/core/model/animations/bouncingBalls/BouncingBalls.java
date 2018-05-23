2382e13097b7b7d8a02c4fe801c8915ebaad03bc
PaulBreugnot <paul.breugnot@etu.emse.fr>
2018-05-23 09:18:46 +0200

    Bouncing Balls

---
+++ src/main/core/model/animations/bouncingBalls/BouncingBalls.java
@@ -0,0 +1,29 @@
+package main.core.model.animations.bouncingBalls;
+
+import java.io.IOException;
+
+import javafx.scene.layout.AnchorPane;
+import main.core.model.animations.Animation;
+import main.core.model.pixel.RGBWPixel;
+
+public class BouncingBalls implements Animation {
+
+	@Override
+	public void setNextPicture(RGBWPixel[][] ledMatrix, int matrixWidth, int matrixHeight) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void setAnimationSettings(AnchorPane configAnchorPane) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Animation newAnimationInstance() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
