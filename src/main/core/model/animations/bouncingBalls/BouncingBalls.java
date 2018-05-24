fa71502594df4d679179dd43b1fe9d0074b333dd
PaulBreugnot <paul.breugnot@etu.emse.fr>
2018-05-24 08:40:08 +0200

    Synchronized display with properties!

--- src/main/core/model/animations/bouncingBalls/BouncingBalls.java
+++ src/main/core/model/animations/bouncingBalls/BouncingBalls.java
@@ -3,6 +3,8 @@ package main.core.model.animations.bouncingBalls;
 import java.io.IOException;
 import java.util.Random;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.scene.layout.AnchorPane;
 import main.core.model.animations.Animation;
 import main.core.model.panel.LedPanel;
@@ -23,6 +25,28 @@ public class BouncingBalls implements Animation {
 			int x = rd.nextInt(LedPanel.MATRIX_WIDTH - 2) + 1;
 			int y = rd.nextInt(LedPanel.MATRIX_HEIGHT - 2) + 1;
 			Particle particle = new Particle(0.5, angle, x, y);
+			particle.xPosProperty().addListener(new ChangeListener<Number>() {
+				public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+					int old_x = (int) Math.floor((double) old_val);
+					int new_x = (int) Math.floor((double) new_val);
+					if (new_x != old_x) {
+						int y = (int) Math.floor(particle.getyPos());
+						bufferLedMatrix[y][new_x] = RGBWPixel.rgbPixel(255, 0, 0);
+						bufferLedMatrix[y][old_x] = RGBWPixel.rgbPixel(0, 0, 0);
+					}
+				}
+			});
+			particle.yPosProperty().addListener(new ChangeListener<Number>() {
+				public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
+					int old_y = (int) Math.floor((double) old_val);
+					int new_y = (int) Math.floor((double) new_val);
+					if (new_y != old_y) {
+						int x = (int) Math.floor(particle.getxPos());
+						bufferLedMatrix[new_y][x] = RGBWPixel.rgbPixel(255, 0, 0);
+						bufferLedMatrix[old_y][x] = RGBWPixel.rgbPixel(0, 0, 0);
+					}
+				}
+			});
 			particleSet.addParticle(particle);
 		}
 	}
@@ -30,14 +54,14 @@ public class BouncingBalls implements Animation {
 	@Override
 	public void setNextPicture(RGBWPixel[][] ledMatrix, int matrixWidth, int matrixHeight) {
 		particleSet.progress(1);
-		System.out.println("Salut!");
-		for (Particle particle : particleSet.getParticles()) {
-			System.out.println("xPos = " + particle.getxPos());
-			System.out.println("yPos = " + particle.getyPos());
-			int x = (int) Math.floor(particle.getxPos());
-			int y = (int) Math.floor(particle.getyPos());
-			ledMatrix[y][x] = RGBWPixel.rgbPixel(255, 0, 0);
-		}
+
+		/*
+		 * for (Particle particle : particleSet.getParticles()) {
+		 * System.out.println("xPos = " + particle.getxPos());
+		 * System.out.println("yPos = " + particle.getyPos()); int x = (int)
+		 * Math.floor(particle.getxPos()); int y = (int) Math.floor(particle.getyPos());
+		 * ledMatrix[y][x] = RGBWPixel.rgbPixel(255, 0, 0); }
+		 */
 
 	}
 
