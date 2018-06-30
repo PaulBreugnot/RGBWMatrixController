82257d09dfa3aff14d8c1f1b874d54d58a783864
Paul Breugnot <paul.breugnot@etu.emse.fr>
2018-06-30 11:44:11 -0500

    Particle that implements comparable, to manage particle layers

---
+++ src/main/core/model/animations/bouncingParticles/simpleBouncingParticles/BouncingParticle.java
@@ -0,0 +1,37 @@
+package main.core.model.animations.bouncingParticles.simpleBouncingParticles;
+
+import java.util.ArrayList;
+
+import javafx.scene.paint.Color;
+import main.core.model.animations.bouncingParticles.bouncingParticlesEngine.particle.Particle;
+
+public class BouncingParticle extends Particle implements Comparable<BouncingParticle> {
+	
+	private ArrayList<BouncingParticle> AboveOf = new ArrayList<>();
+
+	public BouncingParticle(double speed, double alpha, double xPos, double yPos, double radius, Color color) {
+		super(speed, alpha, xPos, yPos, radius, color);
+	}
+	
+	public void addAboveOf(BouncingParticle particle) {
+		AboveOf.add(particle);
+	}
+	
+	public void removeAboveOf(BouncingParticle particle) {
+		AboveOf.remove(particle);
+	}
+	
+	public boolean isAboveOf(BouncingParticle particle) {
+		return AboveOf.contains(particle);
+	}
+	
+
+	@Override
+	public int compareTo(BouncingParticle arg0) {
+		if(AboveOf.contains(arg0)) {
+			return -1;
+		}
+		return 0;
+	}
+
+}
